24677741d9c1d3d462088b5565b2ef09b3a13675
Melissa <wolfaholic@users.noreply.github.com>
2022-05-27 06:49:55 -0700

    saving progress

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,4 @@
-const User = require('..users-model/users/')
+const User = require('../users/users-model')
 
 
 /*
@@ -63,7 +63,7 @@ async function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  if(!req.body.password || req.body.password < 3) {
+  if(!req.body.password || req.body.password.length < 3) {
     next({message: "Password must be longer than 3 chars", status: 422})
   } else {
     next()
